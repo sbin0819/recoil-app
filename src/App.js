e10a5190ce5f8acb6d6edf89680c2bb160f00c73
sbin0819 <sbinha123@gmail.com>
2021-11-19 12:43:56 +0900

    ENH: styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import GlobalStyle from 'styles/GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
+import { Header } from 'components/common';
 import Home from 'pages/Home/Home';
+import Search from 'pages/Search/Search';
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-    </Routes>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/search" element={<Search />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Search(props) {
+  return <div>search</div>;
+}
+
+export default Search;
